a9be1e1808853ac9d1aea78664c2997ec9a7619e
seungho-hub <kmc54320@gmail.com>
2022-07-16 18:23:01 +0900

    mistake..

--- views/src/App.svelte
+++ views/src/App.svelte
@@ -2,15 +2,20 @@
   import Router from "svelte-spa-router";
 
   import Auth from "./auth/Auth.svelte";
+  import HP from "./hp/HP.svelte";
 </script>
 
 <main>
   <Router
     routes={{
+      "/": HP,
       "/auth": Auth,
     }}
   />
 </main>
 
 <style lang="scss">
+  * {
+    color: white;
+  }
 </style>
